46e9550e6580c0985fae29c126b188f8d73dcb4e
Dacayo <russeldacayo48@gmail.com>
2026-08-19 15:20:54 +0800

    first commit

---
+++ script.js
@@ -0,0 +1,39 @@
+const correctUsername = "nita";
+const correctPassword = "mybaby";
+
+
+document.getElementById("loginForm").addEventListener("submit", function(event) {
+
+    event.preventDefault();
+
+    const username =
+        document.getElementById("username").value.trim();
+
+    const password =
+        document.getElementById("password").value;
+
+
+    if (username === "" || password === "") {
+
+        alert("Please enter your username and password.");
+
+        return;
+    }
+
+
+    if (
+        username === correctUsername &&
+        password === correctPassword
+    ) {
+
+        alert("Login successful!");
+
+        window.location.href = "home.html";
+
+    } else {
+
+        alert("Incorrect username or password.");
+
+    }
+
+});
